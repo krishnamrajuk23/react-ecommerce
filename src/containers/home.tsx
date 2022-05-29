985826014ddd2ac709ba7383f293c6043b98a31f
LAPTOP-JQA1MISB\raju <krishnamrajuk23@gmail.com>
2022-05-29 20:45:39 +0530

    button component and storybook setup

--- src/containers/home.tsx
+++ src/containers/home.tsx
@@ -2,14 +2,18 @@ import NavBar from "../components/NavBar";
 import Button from "../theme/components/button/Button";
 import Card from "../theme/components/card/Card";
 
+
 function HomeContainer() {
+  const handleClick = (event: Event) => {
+    console.log("handle Click", event)
+  }
   return (
     <div className="home">
       <NavBar></NavBar>
       <Card title="simple card">
         <p>This is random card</p>
         <div>asdfasfsdafsdf</div>
-        <Button name="view" bType="primary"></Button>
+        <Button name="view" bType="primary" click={handleClick}></Button>
       </Card>
     </div>
   );
--- src/theme/components/button/Button.tsx
+++ src/theme/components/button/Button.tsx
@@ -1,21 +1,23 @@
 import { FC, ReactElement, ReactNode } from 'react';
 import './Button.scss';
 
-interface Button{
-  children?: ReactNode,
-  name: string,
-  bType?: string,
-  disable?: boolean
+interface ButtonProps{
+  children?: ReactNode;
+  name: string;
+  bType?: string;
+  disable?: boolean;
+  size?: 'sm' | 'md' | 'lg';
+  click?: (event: any) => void;
 }
 
-const Button: FC<Button> = ({ children, name, bType, disable }): ReactElement  => {
+const Button = ({ children, name, bType, disable, size = 'sm', click }:ButtonProps): ReactElement  => {
   let btnClass = 'default';
   if (bType?.length) {
     btnClass = bType;
   }
 
-  btnClass = 'btn-' + btnClass;
-  return (<button type='button' className={'btn ' + btnClass} disabled={disable}>
+  btnClass = 'btn-' + btnClass + 'btn-' + size;
+  return (<button type='button' className={'btn ' + btnClass} disabled={disable} onClick={(event) => click && click(event)}>
     { children}
     {name}
   </button>)
